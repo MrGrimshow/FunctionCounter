9200c6df229cfe9faf19249e3008af0683daa2d7
Herbert <grimshowmaino@gmil.com>
2021-08-25 12:34:30 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './Components/ClassCounter';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = { number: 0};
+    }
+  
+    AddOne() {
+      this.setState({
+        number: this.state.number + 1
+      });
+    }
+  
+    SubtractOne(){
+      this.setState({
+        number: this.state.number - 1
+      })
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.number}</h1>
+          <button onClick={() => this.AddOne()}>Add 1</button>
+          <button onClick={() => this.SubtractOne()}>Sub</button>
+        </div>
+      );
+    }
+  }
+
 
 export default App;
---
+++ src/Components/ClassCounter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ClassCounter extends React.Component {
+  render() {
+    return (
+        <button
+        onClick={this.props.handleClick} >
+        {this.props.btnTitle}
+      </button>
+    );
+  }
+}
+
+export default ClassCounter;
